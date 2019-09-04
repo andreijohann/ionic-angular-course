f5bcff70120dc8db5ef6b4d00bbac8efaf94c059
Andrei Johann <andrei_johann@yahoo.com>
2019-09-04 11:43:28 -0300

    Managing State with Service

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-recipes',
@@ -7,22 +8,13 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-  recipes: Recipe[] = [
-    { id: 'r1',
-    title: 'Schnitzel',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
-    ingredients: ['french fries', 'pork meat', 'Salad']
-  },
-  { id: 'r2',
-  title: 'Spaghetti',
-  imageUrl: 'https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg',
-  ingredients: ['Spaghetti', 'meat', 'tomatos']
-}
-  ];
 
-  constructor() { }
+recipes: Recipe[];
+
+  constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
+    this.recipes = this.recipesService.getAllRecipes();
   }
 
 }
---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      ingredients: ['french fries', 'pork meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg',
+      ingredients: ['Spaghetti', 'meat', 'tomatos']
+    }
+  ];
+
+  constructor() { }
+
+  getAllRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId: string) {
+    return {
+      ...this.recipes.find(recipe => {
+        return recipe.id === recipeId;
+      })
+    };
+  }
+
+}
+
