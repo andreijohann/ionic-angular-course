46199d2c8d7f5ebb5a20e82d829a98cdfcf8a8f8
Andrei Johann <andrei_johann@yahoo.com>
2019-09-05 21:33:07 -0300

    Ionic Page Caching & Extra Lifecycle Hooks

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RecipesService } from '../recipes.service';
 import { Recipe } from '../recipe.model';
@@ -9,16 +9,17 @@ import { AlertController } from '@ionic/angular';
   templateUrl: './recipe-detail.page.html',
   styleUrls: ['./recipe-detail.page.scss'],
 })
-export class RecipeDetailPage implements OnInit {
+export class RecipeDetailPage implements OnInit, OnDestroy {
 
   loadedRecipe: Recipe;
 
   constructor(private activatedRoute: ActivatedRoute,
-    private recipesService: RecipesService,
-    private router: Router,
-    private alertCtrl: AlertController) { }
+              private recipesService: RecipesService,
+              private router: Router,
+              private alertCtrl: AlertController) { }
 
   ngOnInit() {
+    console.log('ngOnInit');
     this.activatedRoute.paramMap.subscribe(paramMap => {
       if (!paramMap.has('recipeId')) {
         // redirect
@@ -29,6 +30,27 @@ export class RecipeDetailPage implements OnInit {
       this.loadedRecipe = this.recipesService.getRecipe(recipeId);
     });
   }
+
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter');
+  }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter');
+  }
+
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave');
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
+
 
   onDeleteRecipe() {
     this.alertCtrl.create({
